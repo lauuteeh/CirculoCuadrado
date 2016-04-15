263625960a4155a42c995a4dcbdbdbd3620a3021
julianbermolen <julianbermolen@gmail.com>
2016-04-15 20:25:20 -0300

    bajo los cambios

--- src/cuadrado/Cuadrado.java
+++ src/cuadrado/Cuadrado.java
@@ -2,4 +2,16 @@ package cuadrado;
 
 public class Cuadrado {
 
+		public Integer perimetro(Integer lado){
+			
+			int resul = lado*4;
+			
+			return resul;
+		}
+		public Integer area(Integer lado){
+			
+			int resul = lado*lado;
+			
+			return resul;
+		}
 }
--- test/trianguloTest/TrianguloTest.java
+++ test/trianguloTest/TrianguloTest.java
@@ -1,5 +1,31 @@
 package trianguloTest;
 
-public class TrianguloTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+import triangulo.Triangulo;
+
+
+
+public class TrianguloTest{
+
+	@Test
+	public void perimetroTest() {
+		Triangulo triangulito = new Triangulo();
+		Integer resulEsperado = 16;
+		
+		Integer resulObtenido = triangulito.perimetro(4);
+		
+		Assert.assertEquals(resulEsperado,resulObtenido);
+	}
+
+	@Test
+	public void areaTest() {
+		Triangulo triangulito = new Triangulo();
+		Integer resulEsperado = 16;
+		
+		Integer resulObtenido = triangulito.area(4);
+		
+		Assert.assertEquals(resulEsperado,resulObtenido);
+	}
 }
