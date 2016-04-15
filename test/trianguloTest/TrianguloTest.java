144729176c1ffdcf700e881b93102f8022d23098
julianbermolen <julianbermolen@gmail.com>
2016-04-15 20:34:42 -0300

    Push completo

--- test/trianguloTest/TrianguloTest.java
+++ test/trianguloTest/TrianguloTest.java
@@ -12,29 +12,17 @@ public class TrianguloTest{
 	@Test
 	public void perimetroTest() {
 		Triangulo triangulito = new Triangulo();
-<<<<<<< HEAD
-		Integer resulEsperado = 16;
-=======
+
 		Integer resulEsperado = 12;
->>>>>>> 6b7e1de342e50d2120727547ff3bf4eee48be60a
-		
 		Integer resulObtenido = triangulito.perimetro(4);
-		
 		Assert.assertEquals(resulEsperado,resulObtenido);
 	}
 
 	@Test
 	public void areaTest() {
 		Triangulo triangulito = new Triangulo();
-<<<<<<< HEAD
-		Integer resulEsperado = 16;
-		
-		Integer resulObtenido = triangulito.area(4);
-=======
 		Integer resulEsperado = 4;
-		
-		Integer resulObtenido = triangulito.area(4, 2);
->>>>>>> 6b7e1de342e50d2120727547ff3bf4eee48be60a
+		Integer resulObtenido = triangulito.area(4,2);
 		
 		Assert.assertEquals(resulEsperado,resulObtenido);
 	}
